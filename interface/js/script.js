767c6764ba50db86ada3b40133e61f73cdfb50ca
sarah <sarahgarcin@hotmail.com>
2014-06-19 10:58:23 +0200

    interface - typo + images ...

--- interface/js/script.js
+++ interface/js/script.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
 
-$(".plus1").click(function(){
- 	$('.commentaire1').css("display", "block");
- 	$(".plus1").addClass("plus1active");
-})
+	$(".plus1").click(function(){
+	 	$('.commentaire1').css("display", "block");
+	 	$(".plus1").addClass("plus1active");
+	 	$(".plus1").removeClass("plus1");
+	 		
+	 	$(".plus1active").click(function(){
+		 	$('.commentaire1').css("display", "none");
+		 	$(".plus1active").addClass("plus1");
+		 	$(".plus1active").removeClass("plus1active");		 	
+		});
 
-$(".plus1active").click(function(){
- 	$('.commentaire1').css("display", "none");
- 	$(".plus1active").removeClass("plus1active");
-});
+	});
 
+	$(".plus2").click(function(){
+	 	$('.commentaire2').css("display", "block");
+	 	$(".plus2").addClass("plus2active");
+	 	$(".plus2").removeClass("plus2");
+	 		
+	 	$(".plus2active").click(function(){
+		 	$('.commentaire2').css("display", "none");
+		 	$(".plus2active").addClass("plus2");
+		 	$(".plus2active").removeClass("plus2active");		 	
+		});
 
-
+	});
 
 });
